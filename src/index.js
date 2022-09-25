ac5d7a2635b51c421521dd2bd9312c47c4889f0d
Christian <dltorres862@misena.edu.co>
2022-09-24 21:24:21 -0500

    index

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import Banner from './components/banner/Banner';
 import Descripcion from './components/descripcion/Descripcion';
 import Feature from './components/feature/Feature';
 import Nav from './components/nav/Nav';
+import Ubicanos from './components/ubicanos/Ubicanos';
+import Opiniones from './components/opiniones/Opiniones';
+import Footer from './components/footer/Footer';
 import './index.css';
 
 
@@ -15,6 +18,9 @@ root.render(
     <Banner/>
     <Descripcion/>
     <Feature/>
+    <Ubicanos/>
+    <Opiniones/>
+    <Footer/>
   </React.StrictMode>
 );
 
